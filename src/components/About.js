2ee81804d0a463e927687f57e896bd28bae299a9
Daniel Pham <pduy091885@gmail.com>
2021-10-29 13:03:22 +1000

    about and home updated

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,11 @@ import React from "react";
 const About = () => (
     <div>
         <h2>About</h2>
-        Some details about the web page:
-        +...
-        +...
-        +...
-        +...
+        <h1>Some details about the web page:</h1>
+        <h1>+...</h1>
+        <h1>+...</h1>
+        <h1>+...</h1>
+        <h1>+...</h1>
     </div>
 );
 export default About;
